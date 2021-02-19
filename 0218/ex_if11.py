f768ac8e7b83146b9ab7b6ab41db074f384ede2a
parka01 <68191916+parka01@users.noreply.github.com>
2021-02-19 09:27:50 +0900

    2/19

--- 0218/ex_if11.py
+++ 0218/ex_if11.py
@@ -9,3 +9,16 @@ elif 9<=month<=11:
     print('가을입니다.')
 else:
     print('잘못된 입력입니다.')
+#----------------모범답안---------------------
+month=int(input('현재 월 입력(정수): '))
+if((month<1)or(month>12)):
+    print("잘못된 입력")
+
+elif((month>=3))and(month<=5)):
+    print("봄")
+elif((month>=6))and(month<=8)):
+    print("여름")
+elif((month>=9))and(month<=11)):
+    print("가을")
+else:
+    print("겨울")
--- 0218/ex_if14.py
+++ 0218/ex_if14.py
@@ -5,3 +5,21 @@ if num>0:
     print('입력값',num,'은 양수입니다!')
 if num==0:
     print('입력값',num,'은 0입니다!')
+#----------------모범답안---------------------
+#단순 if문
+num=int(input('숫자입력: '))
+if(num<0):
+    print('입력값 %d는(은) 음수'%num) #%d는 정수형 변수 실수형은 %f
+if(num==0):
+    print('입력값 %d는(은) 0'%num)
+if(num>0):
+    print('입력값 %d는(은) 양수'%num)
+#----------------모범답안---------------------
+#이중if문
+num=int(input('숫자 입력: '))
+if(num<0):
+    print('입력값 %d는(은) 양수'%num)
+elif(num==0):
+    print('입력값 %d는(은) 0'%num)
+else:
+    print('입력값 %d는(은) 양수'%num)
