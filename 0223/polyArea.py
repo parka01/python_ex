9a5fdfc4bfa4dd0eb8a01c40202d358fa910b648
parka01 <68191916+parka01@users.noreply.github.com>
2021-02-23 16:32:51 +0900

    2/23 Module

---
+++ 0223/polyArea.py
@@ -0,0 +1,12 @@
+#모듈 생성
+pi=3.14159
+def recArea(width,depth):
+    return width*depth
+def triArea(base,height):
+    return base*height/2
+
+def cirArea(r):
+    return pi*r*r
+
+def circum(r):
+    return 2*pi*r
