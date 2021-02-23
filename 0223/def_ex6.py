792544fc3c2f72998ed84fcac23a78bee45bc14d
parka01 <68191916+parka01@users.noreply.github.com>
2021-02-23 16:19:54 +0900

    2/23 basic python

--- 0223/def_ex6.py
+++ 0223/def_ex6.py
@@ -2,13 +2,22 @@
 #소수를 출력하는 프로그램을 작성하시오. 두 개의 숫자를 매개변수로
 #받아서 두 숫자 사이의 소수를 출력하고, 소수의 개수를 반환하는
 # 부분을 findPrime(x,y)함수로 작성하시오.
-num=int(input('첫 번째 숫자(작은 수) 입력: '))
-num2=int(input('두 번째 숫자(큰 수) 입력'))
-for i in range(num,num2+1):
-    count = 0
-    
-    for j in range(1,i+1):
-        if i%j == 0:
-            count += 1
-    if count <= 2:
-        print(i)
+def findPrime(x,y):
+    print(x,'부터',y,'사이의 소수: ',end=' ')
+    cnt=0
+    for i in range(x,y+1):
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            print(i,end=' ')
+            cnt=cnt+1
+    else:
+        print('')
+    return cnt
+
+num1=int(input('첫번째 숫자: '))
+num2=int(input('첫번째 숫자: '))
+
+cnt=findPrime(num1,num2) #실매개변수는 num1,num2
+print('소수는 모두 ',cnt,'개 입니다.')
