955b767da8fb16922a75d8a40a7baa9bbbb5f89b
parka01 <68191916+parka01@users.noreply.github.com>
2021-02-24 15:13:12 +0900

    2/24

--- Basic2.py
+++ Basic2.py
@@ -45,4 +45,26 @@ Hi python, Hi python, Hi python, Hi python, Hi python,
 나이는: 19
 나이는19
 <class 'str'> <class 'int'>
->>> 
+>>> for i in range(10):
+	print(i,end=' ')
+
+	
+0 1 2 3 4 5 6 7 8 9 
+>>> for i in range(1,10):
+	print(i,end=' ')
+
+	
+1 2 3 4 5 6 7 8 9 
+>>> #(1,숫자)형식으로 적어야 시작이 1부터이다.
+>>> for i in range(1,10,2):
+	print(i,end=' ')
+
+	
+1 3 5 7 9 
+>>> #1부터 시작해서 10까지 2씩 증가한 값을 출력
+>>> for i in range(10,1,-1):
+	print(i,end=' ')
+
+	
+10 9 8 7 6 5 4 3 2 
+>>> #주의 할 점! 1까지가 아닌 1+1 된 값 까지이다.
