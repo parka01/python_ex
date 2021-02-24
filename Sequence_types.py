79a2d0f4fdc347e1dc84515bd4ab845b0495465b
parka01 <68191916+parka01@users.noreply.github.com>
2021-02-24 16:25:29 +0900

    2/24 Sequence types

---
+++ Sequence_types.py
@@ -0,0 +1,14 @@
+Python 3.8.3 (v3.8.3:6f8c8320e9, May 13 2020, 16:29:34) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #List:
+>>> list1=['kim','lee','park','jung',1,2,3,4]
+>>> #Tuple:
+>>> tuple1=('computer',3,202133345,'Ellia')
+>>> #Set:
+>>> set1={'fruit',5,1004}
+>>> type(set)
+<class 'set'>
+>>> #Dictionary:
+>>> dict1={1:'a',2:'b',3:'c'}
+>>> 
